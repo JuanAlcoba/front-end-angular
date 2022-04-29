bd21c7da9e8625c0fd2e6335642019fea19c727f
Juan Alcoba <jc.alcoba@gmail.com>
2022-04-29 10:56:12 -0300

    Maqueta pasada a Angular, secciones separadas por componentes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { PresentacionComponent } from './presentacion/presentacion.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { EducacionComponent } from './educacion/educacion.component';
+import { HabilidadesComponent } from './habilidades/habilidades.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { ContactameComponent } from './contactame/contactame.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    PresentacionComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HabilidadesComponent,
+    ProyectosComponent,
+    ContactameComponent
   ],
   imports: [
     BrowserModule,
