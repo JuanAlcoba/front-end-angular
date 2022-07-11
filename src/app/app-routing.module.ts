a2ffd724f432f86a5a8d972d8c7cd02ca3ffba23
Juan Alcoba <jc.alcoba@gmail.com>
2022-07-11 16:26:59 -0300

    asd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { CrearComponent } from './components/educacion/crear/crear.component';
 import { EditarComponent } from './components/educacion/editar/editar.component';
+import { PresentacionComponent } from './components/presentacion/presentacion.component';
 
 const routes: Routes = [
   {path:'', component:AppComponent},
   {path:'editar', component:EditarComponent},
-  {path:'crear', component:CrearComponent}
+  {path:'crear', component:CrearComponent},
+  {path:'presentacion', component:PresentacionComponent}
+
 ];
 
 @NgModule({
